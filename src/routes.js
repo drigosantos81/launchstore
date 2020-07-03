df92c9d1e7e0a79abd74f0ff9f72856c4143fea3
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-03 14:56:27 -0300

    Criada página de cadastro de produto e sua rota.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const routes = express.Router();
+const productsController = require('./app/controllers/products');
 
 routes.get('/', function(req, res) {
     return res.render("layout.njk");
 });
 
+routes.get('/products/create', productsController.create);
 
+routes.get('/ads/create', function(req, res) {
+    return res.redirect("/products/create");
+});
 
 module.exports = routes;
