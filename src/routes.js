59dafcc0cc8cc95358e0b5944e00795ed7c8c823
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-03 16:56:35 -0300

    Inserção de dados de novo produto no banco de dados.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const routes = express.Router();
-const productsController = require('./app/controllers/products');
+const productsController = require('./app/controllers/productController');
 
 routes.get('/', function(req, res) {
     return res.render("layout.njk");
 });
 
 routes.get('/products/create', productsController.create);
+routes.post('/products', productsController.post);
 
+// ALIAS
 routes.get('/ads/create', function(req, res) {
     return res.redirect("/products/create");
 });
