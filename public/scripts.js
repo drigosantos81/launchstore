64efb7ad84f64653f8fe6aa0aab83f34eab6a55b
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-05 16:01:00 -0300

    Add máscara para valores (Reais) no cadastro de um produto.

--- public/scripts.js
+++ public/scripts.js
@@ -1,66 +1,15 @@
-const currentPage = location.pathname;
-const menuItens = document.querySelectorAll(".links a");
+const Mask = {
+    apply(input, func) {
+        setTimeout(function() {
+            input.value = Mask[func](input.value);
+        }, 1);
+    },
+    formatBRL(value) {
+        value = value.replace(/\D/g,"");
 
-for (item of menuItens) {
-    if (currentPage.includes(item.getAttribute("href"))) {
-        item.classList.add("active");
-    };
-};
-
-function paginate(selectedPage, totalPages) {
-    let pages = [],
-        oldPage
-
-    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-
-        const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
-        const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
-        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
-        
-        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
-            
-            if (oldPage && currentPage - oldPage > 2) {
-                pages.push('...');
-            };
-
-            if (oldPage && currentPage - oldPage == 2) {
-                pages.push(oldPage + 1);
-            }
-
-            pages.push(currentPage);
-
-            oldPage = currentPage;
-        }
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100); 
     }
-
-    return pages;
-}
-
-function createPagination(pagination) {    
-    const filter = pagination.dataset.filter;
-    const page = +pagination.dataset.page;
-    const total = +pagination.dataset.total;
-    const pages = paginate(page, total);
-
-    let elements = '';
-
-    for (let page of pages) {
-        if (String(page).includes('...')) {
-            elements += `<span${page}">${page}</span>`;
-        } else {
-            if (filter) {
-                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`;
-            } else {
-                elements += `<a href="?page=${page}">${page}</a>`;
-            }        
-        }    
-    }
-
-    pagination.innerHTML = elements;
-}
-
-const pagination = document.querySelector('.pagination');
-
-if (pagination) {
-    createPagination(pagination);
 }
