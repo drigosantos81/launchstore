e758b060a5b77903560e85217e40c8256c98d32c
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-21 18:45:13 -0300

    Incluida regras de gerenciamento do carregamento das imagens.

--- public/scripts.js
+++ public/scripts.js
@@ -15,16 +15,12 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event) {
         const { files: fileList } = event.target;
-        const { uploadLimit } = PhotosUpload;
-
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`);
-            event.preventDefault();
-            return
-        }
+        
+        if (PhotosUpload.hasLimit(event)) return;
 
         Array.from(fileList).forEach(file => {
             const reader = new FileReader()
@@ -33,17 +29,33 @@ const PhotosUpload = {
                 const image = new Image();
                 image.src = String(reader.result);
 
-                const div = document.createElement('div');
-                div.classList.add('photo');
+                const div = PhotosUpload.getContainer(image);
 
-                div.onclick = () => alert('Remover foto');
-
-                div.appendChild(image);
-
-                document.querySelector('#photos-preview').appendChild(div);
+                PhotosUpload.preview.appendChild(div);
             }
 
             reader.readAsDataURL(file);
         })
+    },
+    hasLimit(event) {
+        const { uploadLimit } = PhotosUpload;
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`);
+            event.preventDefault();
+            return true;
+        }
+
+        return false;
+    },
+    getContainer(image) {
+        const div = document.createElement('div');
+        div.classList.add('photo');
+
+        div.onclick = () => alert('Remover foto');
+
+        div.appendChild(image);
+
+        return div;
     }
 }
