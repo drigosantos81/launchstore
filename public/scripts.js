1e6a012fb915b5738b89cc5dacbf8ef940033bb6
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-26 20:31:46 -0300

    Estilizado exclusão das imagens a serem carregadas.

--- public/scripts.js
+++ public/scripts.js
@@ -17,12 +17,16 @@ const Mask = {
 const PhotosUpload = {
     preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
+    files: [],
     handleFileInput(event) {
         const { files: fileList } = event.target;
         
-        if (PhotosUpload.hasLimit(event)) return;
+        if (PhotosUpload.hasLimit(event)) return
 
         Array.from(fileList).forEach(file => {
+
+            PhotosUpload.files.push(file);
+
             const reader = new FileReader()
 
             reader.onload = () => {
@@ -30,23 +34,31 @@ const PhotosUpload = {
                 image.src = String(reader.result);
 
                 const div = PhotosUpload.getContainer(image);
-
                 PhotosUpload.preview.appendChild(div);
             }
 
             reader.readAsDataURL(file);
-        })
+        });
+
+        event.target.files = PhotosUpload.getAllFiles();
     },
     hasLimit(event) {
         const { uploadLimit } = PhotosUpload;
-
+        const { files: fileList } = event.target;
+        
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${uploadLimit} fotos`);
             event.preventDefault();
             return true;
         }
-
         return false;
+    },
+    getAllFiles() {
+        const dataTransfer = new Clipboard("").clipboardData || new DataTransfer();
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file));
+
+        return dataTransfer.files;
     },
     getContainer(image) {
         const div = document.createElement('div');
@@ -56,6 +68,14 @@ const PhotosUpload = {
 
         div.appendChild(image);
 
+        div.appendChild(PhotosUpload.getRemoveButton());
+
         return div;
+    },
+    getRemoveButton() {
+        const button = document.createElement('i');
+        button.classList.add('material-icons');
+        button.innerHTML = 'close';
+        return button;
     }
 }
