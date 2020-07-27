d3b7044d8b2935cf2ba5372685905bf56fc572d0
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-27 11:48:58 -0300

    Deleção de imagens no banco e no Frontend implementados. Update sempre apresentando mensagem de campo vazio.

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -70,9 +70,27 @@ module.exports = {
         const keys = Object.keys(req.body);
 
         for (key of keys) {
-            if (req.body[key] == "") {
+            if (req.body[key] == "" && key != "removed_files") {
                 return res.send("Por favor, preencha todos os campos.");
             }
+        }
+
+        if (req.files.length != 0) {
+            const newFilesPromise = req.files.map(file =>
+                File.create({ ...file, product_id: req.body.id}));
+
+            await Promise.all(newFilesPromise);
+        }
+
+        if (req.body.removed_files) {
+            // 1,2,3
+            const removedFiles = req.body.removed_files.split(","); // [1,2,3,]
+            const lastIndex = removedFiles.length - 1;
+            removedFiles.splice(lastIndex, 1); // [1,2,3]
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id));
+
+            await Promise.all(removedFilesPromise);
         }
 
         req.body.price = req.body.price.replace(/\D/g, "");
