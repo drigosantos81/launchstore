9abe7d3c1f68e8e43c2bd4b93f1a62fc4e2a5658
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-27 08:52:52 -0300

    Adicionado multer e pasta images dentro de public.

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const routes = express.Router();
+
+const multer = require('./app/middlewares/multer');
 const productsController = require('./app/controllers/productController');
 
 routes.get('/', function(req, res) {
-    return res.render("layout.njk");
+    return res.render('layout.njk');
 });
 
 routes.get('/products/create', productsController.create);
-routes.post('/products', productsController.post);
 routes.get('/products/:id/edit', productsController.edit);
-routes.put('/products', productsController.put);
+
+routes.post('/products', multer.array('photos', 6), productsController.post);
+routes.put('/products', multer.array('photos', 6), productsController.put);
 routes.delete('/products', productsController.delete);
 
 // ALIAS
