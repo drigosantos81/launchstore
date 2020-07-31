b235fa166d4307a9cd555031db766ff8b8ef2907
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-30 21:44:54 -0300

    Click para exibição da imagem do produto na tela principal de um produto

--- public/scripts.js
+++ public/scripts.js
@@ -128,3 +128,16 @@ const PhotosUpload = {
         PhotosUpload.input.files = PhotosUpload.getAllFiles();
     }
 }
+
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery-preview img'),
+    setImage(e) {
+        const { target } = e;
+
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'));
+        target.classList.add('active');
+
+        ImageGallery.highlight.src = target.src;
+    }
+}
