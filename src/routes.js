c4f98d3a8484a0024fb49db5cb679cfe0eb99c38
Rodrigo Emanuel <drigosantos@gmail.com>
2020-08-19 16:37:41 -0300

    Add parts de .njk para página search.njk e implementado macros de NunJucks.

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,15 @@ const routes = express.Router();
 const multer = require('./app/middlewares/multer');
 const productsController = require('./app/controllers/productController');
 const HomeController = require('./app/controllers/HomeController');
+const SearchController = require('./app/controllers/SearchController');
 
+// HOME
 routes.get('/', HomeController.index);
 
+// SEARCH
+routes.get('/products/search', SearchController.index);
+
+// PRODUCTS
 routes.get('/products/create', productsController.create);
 routes.get('/products/:id', productsController.show);
 routes.get('/products/:id/edit', productsController.edit);
