edd4dcc2b23a72ed7fab55e91295c958f6e3b72d
Rodrigo Emanuel <drigosantos@gmail.com>
2021-01-18 14:43:25 -0300

    Add rota e funções de login.

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,12 +4,13 @@ const routes = express.Router();
 const SessionController = require('../app/controllers/SessionController');
 const UserController = require('../app/controllers/UserController');
 
-const Validator = require('../app/validators/user');
+const UserValidator = require('../app/validators/user');
+const SessionValidator = require('../app/validators/session');
 
 // Login/Logout: SessionController
-// routes.get('/login', SessionController.loginForm);
-// routes.post('/login'), SessionController.login;
-// routes.post('/logout', SessionController.logout);
+routes.get('/login', SessionController.loginForm);
+routes.post('/login', SessionValidator.login, SessionController.login);
+routes.post('/logout', SessionController.logout);
 
 // // Reset password / Forgot: SessionController
 // routes.get('/forgot-password', SessionController.forgotForm);
@@ -19,10 +20,10 @@ const Validator = require('../app/validators/user');
 
 // // User register: UserController
 routes.get('/register', UserController.registerForm);
-routes.post('/register', Validator.post, UserController.post);
+routes.post('/register', UserValidator.post, UserController.post);
 
-routes.get('/', Validator.show, UserController.show);
-routes.put('/', Validator.update, UserController.update);
+routes.get('/', UserValidator.show, UserController.show);
+routes.put('/', UserValidator.update, UserController.update);
 // routes.delete('/', UserController.delete);
 
 module.exports = routes;
