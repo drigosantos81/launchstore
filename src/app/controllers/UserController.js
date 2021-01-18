3cbdf0fa4735c558753b7f359b4f6b42f9b2058d
Rodrigo Emanuel <drigosantos@gmail.com>
2021-01-18 10:31:10 -0300

    Add função update do usuário.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,20 +16,41 @@ module.exports = {
     },
 
     async show(req, res) {
-        const { userId: id } = req.session;
-
-        const user = await User.findOne({ where: {id} });
-
-        if (!user) {
-            return res.render("user/register"), {
-                error: 'Usuário não encontrado'
-            };
-        }
+        const { user } = req;
 
         user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj);
         user.cep = formatCep(user.cep);
 
         return res.render('user/index', { user });
+    },
+
+    async update(req, res) {
+        try {
+            const { user } = req;
+
+            let { name, email, cpf_cnpj, cep, address } = req.body;
+            
+            cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
+            cep = cep.replace(/\D/g, "");
+
+            await User.update(user.id, {
+                name,
+                email,
+                cpf_cnpj,
+                cep,
+                address
+            });
+
+            return res.render('user/index', {
+                user: req.body,
+                success: 'Conta atualizada com sucesso.'
+            });
+        } catch (error) {
+            console.error(error);
+            return res.render('user/index', {
+                error: 'Algum erro aconteceu'
+            });
+        }
     }
     
 }
