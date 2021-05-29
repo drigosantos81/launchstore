f0e730c33526c3ecb2bff2d063a5288052d53044
Rodrigo Emanuel <drigosantos@gmail.com>
2021-05-29 12:06:30 -0300

    Add "lib" para gerenciar carrinho de compras.

---
+++ src/lib/cart.js
@@ -0,0 +1,131 @@
+const { formatPrice } = require('./utils');
+
+// Carrinho salvo na sessão (req.session)
+const Cart = {
+  init(oldCart) {
+    if (oldCart) {
+      this.items = oldCart.items; // => [{ product: {}, price, quantity, formattedPrice }, {}]
+      this.total = oldCart.total;
+    } else {
+      this.items = [];
+      this.total = {
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0)
+      }
+    }
+
+    return this;
+  },
+
+  // Adicionar 1 item
+  addOne(product) {
+    // Verifica se o produto já existe no carrinho
+    let inCart = this.items.find(item => item.product.id == product.id);
+
+    // Se o produto ainda não existir
+    if (!inCart) {
+      inCart = {
+        product: {
+          ...product,
+          formattedPrice: formatPrice(product.price)
+        },
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0)
+      }
+
+      this.items.push(inCart);
+    }
+
+    // Verifica o estoque(quantity)
+    if (inCart.quantity >= product.quantity) {
+      return this;
+    }
+
+    // Atualiza o item
+    inCart.quantity++;
+    inCart.price = inCart.product.price * inCart.quantity;
+    inCart.formattedPrice = formatPrice(inCart.price);
+
+    // Atualiza o Cart
+    this.total.quantity++;
+    this.total.price += inCart.product.price;
+    this.total.formattedPrice = formatPrice(this.total.price);
+
+    return this;
+  },
+
+  // Remover 1 item
+  removeOne(productId) {
+    // Pegar o item do carrinho
+    let inCart = this.items.find(item => item.product.id == productId);
+
+    if (!inCart) {
+      return this;
+    }
+
+    // Atualiza o item
+    inCart.quantity--;
+    inCart.price = inCart.product.price * inCart.quantity;
+    inCart.formattedPrice = formatPrice(inCart.price);
+
+    // Atualiza o Cart
+    this.total.quantity--;
+    this.total.price -= inCart.product.price;
+    this.total.formattedPrice = formatPrice(this.total.price);
+
+    if (inCart.quantity < 1) {
+      this.items = this.items.filter(item => 
+          item.product.id !=inCart.product.id
+        );
+      // Versão 1
+      // const itemIndex = this.items.indexOf(inCart);
+      // this.items.splice(itemIndex, 1);
+
+      return this;
+    }
+
+    return this;
+  },
+
+  // Deletar todo o item
+  delete(productId) {}
+}
+
+// ==== TESTES ====
+const product = {
+  id: 1,
+  price: 199,
+  quantity: 2
+}
+
+const product2 = {
+  id: 2,
+  price: 299,
+  quantity: 1
+}
+
+console.log('Add FIRST cart item');
+let oldCart = Cart.init().addOne(product);
+console.log(oldCart);
+
+console.log('Add SECOND cart item');
+oldCart = Cart.init(oldCart).addOne(product);
+console.log(oldCart);
+
+console.log('Add THIRD cart item');
+oldCart = Cart.init(oldCart).addOne(product2);
+console.log(oldCart);
+
+console.log('remove one cart item');
+oldCart = Cart.init(oldCart).removeOne(product.id);
+console.log(oldCart);
+
+console.log('remove one cart item');
+oldCart = Cart.init(oldCart).removeOne(product.id);
+console.log(oldCart);
+
+// ==== FINAL TESTES ====
+
+module.exports = Cart;
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,104 +1,102 @@
 const Intl = require('intl');
 
 module.exports = {
-    age(timestamp) {
-        
-        const today = new Date();
-        const birthDate = new Date(timestamp);
-    
-        // Ano: 2019 - 1985 = 34
-        let age = today.getFullYear() - birthDate.getFullYear();
-        const month = today.getMonth() - birthDate.getMonth();
-    
-        // Mês: 11 - 12 = -1
-        // Mês: 11 - 11 = 0
-        if (month < 0 ||
-            month == 0 &&
-            today.getDate() <= birthDate.getDate()) {
-                age = age - 1;
-        }
-    
-        return age;
-    },
+	age(timestamp) {
+		const today = new Date();
+		const birthDate = new Date(timestamp);
 
-    date(timestamp) {
-        const date = new Date(timestamp);
+		// Ano: 2019 - 1985 = 34
+		let age = today.getFullYear() - birthDate.getFullYear();
+		const month = today.getMonth() - birthDate.getMonth();
 
-        const year = date.getFullYear();
-        const month = `0${date.getMonth() + 1}`.slice(-2);
-        const day = `0${date.getDate()}`.slice(-2);
-        const hour = date.getHours();
-        const minutes = date.getMinutes();
+		// Mês: 11 - 12 = -1
+		// Mês: 11 - 11 = 0
+		if (month < 0 ||
+			month == 0 &&
+			today.getDate() <= birthDate.getDate()) {
+				age = age - 1;
+		}
 
-        return {
-            day,
-            month,
-            year,
-            hour,
-            minutes,
-            iso: `${year}-${month}-${day}`,
-            format: `${day}/${month}/${year}`
-        }
-    },
+		return age;
+	},
 
-    birthDay(timestamp) {
+	date(timestamp) {
+		const date = new Date(timestamp);
 
-        const date = new Date(timestamp);
+		const year = date.getFullYear();
+		const month = `0${date.getMonth() + 1}`.slice(-2);
+		const day = `0${date.getDate()}`.slice(-2);
+		const hour = date.getHours();
+		const minutes = date.getMinutes();
 
-        const year = date.getFullYear();
-        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
-        const day = `0${date.getUTCDate()}`.slice(-2);
+		return {
+			day,
+			month,
+			year,
+			hour,
+			minutes,
+			iso: `${year}-${month}-${day}`,
+			format: `${day}/${month}/${year}`
+		}
+	},
 
-        // Padrão de data no HTML: yyyy-mm-dd
-        return {
-            day,
-            month,
-            year,
-            iso: `${day}/${month}`,
-            birthDate: `${month}/${year}`,
-            format: `${day}/${month}/${year}`
-        };
-    },
+	birthDay(timestamp) {
+		const date = new Date(timestamp);
 
-    formatPrice(price) {
-        return new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-        }).format(price/100);
-    },
+		const year = date.getFullYear();
+		const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+		const day = `0${date.getUTCDate()}`.slice(-2);
 
-    formatCpfCnpj(value) {
-        value = value.replace(/\D/g,"");
+		// Padrão de data no HTML: yyyy-mm-dd
+		return {
+			day,
+			month,
+			year,
+			iso: `${day}/${month}`,
+			birthDate: `${month}/${year}`,
+			format: `${day}/${month}/${year}`
+		};
+	},
 
-        if (value.length > 14) {
-            value = value.slice(0, -1);
-        }
+	formatPrice(price) {
+		return new Intl.NumberFormat('pt-BR', {
+			style: 'currency',
+			currency: 'BRL'
+		}).format(price/100);
+	},
 
-        // Checa se é CPF ou CNPJ
-        if (value.length > 11) {
-            value = value.replace(/(\d{2})(\d)/, "$1.$2"); // 11.222333444455
-            value = value.replace(/(\d{3})(\d)/, "$1.$2"); // 11.222.333444455
-            value = value.replace(/(\d{3})(\d)/, "$1/$2"); // 11.222.333/444455
-            value = value.replace(/(\d{4})(\d)/, "$1-$2"); // 11.222.333/4444-55
-        } else {
-            value = value.replace(/(\d{3})(\d)/, "$1.$2"); // 111.22233344
-            value = value.replace(/(\d{3})(\d)/, "$1.$2"); // 11.222.33344
-            value = value.replace(/(\d{3})(\d)/, "$1-$2"); // 11.222.333-44
-        }
+	formatCpfCnpj(value) {
+		value = value.replace(/\D/g,"");
 
-        return value;
-    },
+		if (value.length > 14) {
+			value = value.slice(0, -1);
+		}
 
-    formatCep(value) {
-        value = value.replace(/\D/g,"");
+		// Checa se é CPF ou CNPJ
+		if (value.length > 11) {
+			value = value.replace(/(\d{2})(\d)/, "$1.$2"); // 11.222333444455
+			value = value.replace(/(\d{3})(\d)/, "$1.$2"); // 11.222.333444455
+			value = value.replace(/(\d{3})(\d)/, "$1/$2"); // 11.222.333/444455
+			value = value.replace(/(\d{4})(\d)/, "$1-$2"); // 11.222.333/4444-55
+		} else {
+			value = value.replace(/(\d{3})(\d)/, "$1.$2"); // 111.22233344
+			value = value.replace(/(\d{3})(\d)/, "$1.$2"); // 11.222.33344
+			value = value.replace(/(\d{3})(\d)/, "$1-$2"); // 11.222.333-44
+		}
 
-        if (value.length > 8) {
-            value = value.slice(0, -1);
-        }
+		return value;
+	},
 
-        value = value.replace(/(\d{2})(\d)/, "$1.$2"); // 11.222333
-        value = value.replace(/(\d{3})(\d)/, "$1-$2"); // 11.222-333
+	formatCep(value) {
+		value = value.replace(/\D/g,"");
 
-        return value;
-    }
+		if (value.length > 8) {
+			value = value.slice(0, -1);
+		}
+
+	value = value.replace(/(\d{2})(\d)/, "$1.$2"); // 11.222333
+	value = value.replace(/(\d{3})(\d)/, "$1-$2"); // 11.222-333
+
+		return value;
+	}
 }
