f1ae4c7c04f9ae1f81730f096ef182e4fecf652d
Rodrigo Emanuel <drigosantos@gmail.com>
2021-05-31 09:28:01 -0300

    Add funções de adicinar e remover um item do carrinho na página.

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -1,3 +1,4 @@
+const { addOne } = require('../../lib/cart');
 const Cart = require('../../lib/cart');
 
 const LoadProductsService = require('../services/LoadProductService');
@@ -5,17 +6,67 @@ const LoadProductsService = require('../services/LoadProductService');
 module.exports = {
 	async index(req, res) {
 		try {
-			const product = await LoadProductsService.load('product', { where: { id: 23 } });
-
+			// Pega o carrinho da sessão
 			let { cart } = req.session;
 
 			//Gerenciador do carrinho
-			cart = Cart.init(cart).addOne(product);
+			cart = Cart.init(cart);
 
 			return res.render("cart/index", { cart });
 		}        
 		catch(err) {
 			console.error(err);
+		}
+	},
+
+	async addOne(req, res) {
+		try {
+			// Pegar id do produto
+			const { id } = req.params;
+			
+			const product = await LoadProductsService.load('product', { where: { id }});
+
+			// Pegar o carrinho da sessão
+			let { cart } = req.session;
+			
+			// Adicionar o produto ao carrinho (usando o gerenciador de carrinho)
+			cart = Cart.init(cart).addOne(product);
+
+			// Atualizar o carrinho da sessão
+			req.session.cart = cart;
+
+			// Redirecionar o usuário para a tela do carrinho
+			return res.redirect('/cart');
+		}
+		catch (err) {
+			console.log(err);
+		}
+	},
+	
+	async removeOne(req, res) {
+		try {
+			// Pegar id do produto
+			let { id } = req.params;
+
+			// Pegar o carrinho da sessão
+			let { cart } = req.session;
+
+			// Se não tiver carrinho, retornar
+			if (!cart) {
+				return req.redirect('/cart');
+			}
+
+			// Iniciar o carrinho removendo o item (gerenciador de carrinho)
+			cart = Cart.init(cart).removeOne(id);
+
+			// Atualiza o carrinho, removendo 1 item
+			req.session.cart = cart;
+
+			// Redirecionar o usuário para a tela do carrinho
+			return res.redirect('/cart');
+		} 
+		catch (err) {
+			console.log(err);
 		}
 	}
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,4 +6,7 @@ const CartController = require('../app/controllers/CartController');
 /* ==== LOGIN/LOGOUT: CartController ==== */
 routes.get('/', CartController.index);
 
+routes.post('/:id/add-one', CartController.addOne);
+routes.post('/:id/remove-one', CartController.removeOne);
+
 module.exports = routes;
